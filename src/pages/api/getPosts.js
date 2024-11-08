bdf98f0b75709ec5d19dd29254b03cf8bd2dacad
srikanthsamaraweera <samaraweeraster@gmail.com>
2024-11-08 12:32:01 -0500

    added search param to backend get post

--- src/pages/api/getPosts.js
+++ src/pages/api/getPosts.js
@@ -4,47 +4,33 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
-    if (req.method === 'GET') {
-        const { page = 1, limit = 20 } = req.query;
+    const { page = 1, limit = 20, search = "" } = req.query;
+    const offset = (page - 1) * limit;
 
-        const pageNumber = parseInt(page, 10);
-        const limitNumber = parseInt(limit, 10);
+    try {
+        const whereClause = search
+            ? {
+                OR: [
+                    { title: { contains: search.toLowerCase() } },
+                    { description: { contains: search.toLowerCase() } },
+                ],
+            }
+            : {};
 
-        try {
-            const totalPosts = await prisma.post.count(); // Get total count for pagination
+        const posts = await prisma.post.findMany({
+            where: whereClause,
+            skip: parseInt(offset),
+            take: parseInt(limit),
+        });
 
-            const posts = await prisma.post.findMany({
-                skip: (pageNumber - 1) * limitNumber,
-                take: limitNumber,
-                select: {
-                    id: true,
-                    title: true,
-                    enabled: true,
-                    description: true,
-                    email: true,
-                    user_email: true,
-                    category: true,
-                    phone: true,
-                    map: true,
-                    date: true,
-                    image1: true,
-                    image2: true
-                }
-            });
+        const totalPosts = await prisma.post.count({ where: whereClause });
+        const totalPages = Math.ceil(totalPosts / limit);
 
-            res.status(200).json({
-                posts,
-                totalPosts,
-                totalPages: Math.ceil(totalPosts / limitNumber),
-                currentPage: pageNumber,
-            });
-        } catch (error) {
-            console.error("Error fetching posts:", error);
-            res.status(500).json({ error: "Failed to fetch posts" });
-        } finally {
-            await prisma.$disconnect();
-        }
-    } else {
-        res.status(405).json({ error: 'Method not allowed' });
+        res.status(200).json({ posts, totalPages });
+    } catch (error) {
+        console.error("Error fetching posts:", error);
+        res.status(500).json({ error: "Internal server error" });
+    } finally {
+        await prisma.$disconnect();
     }
 }
